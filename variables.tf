0e22b84d38ae3a9ecf11f9522c90f394e2903e21
Andrii Maslovskyi <maslovskyi@duck.com>
2023-06-07 22:59:57 +0300

    fix:sensitive

--- variables.tf
+++ variables.tf
@@ -6,9 +6,11 @@ variable "region" {
 variable "access_key" {
   type        = string
   description = "aws access key"
+  sensitive   = true
 }
 
 variable "secret_key" {
   type        = string
   description = "aws secret key"
+  sensitive   = true
 }
